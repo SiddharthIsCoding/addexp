662b943c27b8351b4dd0b38bf47d973bc472c23b
Siddharth Mishra <siddharthbigshows@gmail.com>
2024-11-05 21:02:34 +0530

    Basic prototype

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+function opencreate()
+{
+    document.getElementById("create").style.display = "block";
+    document.getElementById("complimentary").style.display = "block";
+}
+
+function closecreate()
+{
+    document.getElementById("create").style.display = "none";
+    document.getElementById("complimentary").style.display = "none";
+}
